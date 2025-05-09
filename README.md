50ad9dc07b4a1df86ad6525f9a121d2c2737a1ff
snector <52136318+snector@users.noreply.github.com>
2025-05-09 20:23:41 +0530

    Update plugin to version 1.21
    update
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/herby2212/FakePlayers?shareId=XXXX-XXXX-XXXX-XXXX).

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FakePlayers
-##### Supported MC versions: 1.8, 1.9, 1.10, 1.11, 1.12, 1.13, 1.14, 1.15, 1.16, 1.17, 1.18, 1.19
+##### Supported MC versions: 1.8, 1.9, 1.10, 1.11, 1.12, 1.13, 1.14, 1.15, 1.16, 1.17, 1.18, 1.19, 1.21
 
 ## General
 FakePlayers is a Spigot plugin which creates fake players on your server including a name and tablist slot (**advanced mode**).
@@ -14,6 +14,9 @@ The plugin can also be used in a **simple mode** were only the online player cou
 **Requires the TTA plugin** which can be found here: https://github.com/herby2212/TTA
 
 More informations (Commands, Permissions, Setup) will soon follow in the wiki.
+
+### Help Command
+A new help command has been added to the plugin. You can use the `/help` command to display available commands and their descriptions.
 
 ### Need help?
 Feel free to create an [Issue](https://github.com/herby2212/FakePlayers/issues) or visit the discord channel: https://discord.gg/Q4BNfu4y3K
--- src/de/Herbystar/FakePlayers/Commands.java
+++ src/de/Herbystar/FakePlayers/Commands.java
@@ -8,56 +8,72 @@ import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd,String label, String[] args) {				
-		Player p = null;
-		if(sender instanceof Player) {
-			p = (Player) sender;
-			
-			if(cmd.getName().equalsIgnoreCase("fakePlayers")) {
-				if(Main.instance.protocolLib == true) {
-					p.sendMessage(Main.instance.internalPrefix + "§aProtocolLib§e§l Mode! §c§lCommands are not supported in this mode!");
-					return true;
-				}
-				if(args.length == 2) {
-					if(args[0].equalsIgnoreCase("create")) {
-						if(p.hasPermission("FakePlayers.Commands")) {
-							Main.instance.playerListHandler.addCustomOnlinePlayer(args[1]);
-							p.sendMessage(Main.instance.internalPrefix + "§aPlayer successful created!");
-							return true;
-						}
-					}
-					if(args[0].equalsIgnoreCase("remove")) {
-						if(p.hasPermission("FakePlayers.Commands")) {
-							Main.instance.playerListHandler.removeCustomOnlinePlayer(args[1]);
-							p.sendMessage(Main.instance.internalPrefix + "§aPlayer successful removed!");
-							return true;
-						}
-					}
-			
-				}
-			}	
-		} else {
-			if(cmd.getName().equalsIgnoreCase("fakePlayers")) {
-				if(Main.instance.protocolLib == true) {
-					Bukkit.getConsoleSender().sendMessage(Main.instance.internalPrefix + "§aProtocolLib§e§l Mode! §c§lCommands are not supported in this mode!");
-					return true;
-				}
-				if(args.length == 2) {
-					if(args[0].equalsIgnoreCase("create")) {
-						Main.instance.playerListHandler.addCustomOnlinePlayer(args[1]);
-						Bukkit.getConsoleSender().sendMessage(Main.instance.internalPrefix + "§aPlayer successful created!");
-						return true;
-					}
-					if(args[0].equalsIgnoreCase("remove")) {
-						Main.instance.playerListHandler.removeCustomOnlinePlayer(args[1]);
-						Bukkit.getConsoleSender().sendMessage(Main.instance.internalPrefix + "§aPlayer successful removed!");
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = null;
+        if (sender instanceof Player) {
+            p = (Player) sender;
 
+            if (cmd.getName().equalsIgnoreCase("fakePlayers")) {
+                if (Main.instance.protocolLib == true) {
+                    p.sendMessage(Main.instance.internalPrefix + "§aProtocolLib§e§l Mode! §c§lCommands are not supported in this mode!");
+                    return true;
+                }
+                if (args.length == 2) {
+                    if (args[0].equalsIgnoreCase("create")) {
+                        if (p.hasPermission("FakePlayers.Commands")) {
+                            Main.instance.playerListHandler.addCustomOnlinePlayer(args[1]);
+                            p.sendMessage(Main.instance.internalPrefix + "§aPlayer successful created!");
+                            return true;
+                        }
+                    }
+                    if (args[0].equalsIgnoreCase("remove")) {
+                        if (p.hasPermission("FakePlayers.Commands")) {
+                            Main.instance.playerListHandler.removeCustomOnlinePlayer(args[1]);
+                            p.sendMessage(Main.instance.internalPrefix + "§aPlayer successful removed!");
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            if (cmd.getName().equalsIgnoreCase("help")) {
+                if (p.hasPermission("FakePlayers.Commands")) {
+                    p.sendMessage("§aAvailable commands:");
+                    p.sendMessage("§a/fakePlayers create <name> - Creates a fake player with the specified name.");
+                    p.sendMessage("§a/fakePlayers remove <name> - Removes the fake player with the specified name.");
+                    p.sendMessage("§a/help - Displays help information for FakePlayers plugin.");
+                    return true;
+                }
+            }
+        } else {
+            if (cmd.getName().equalsIgnoreCase("fakePlayers")) {
+                if (Main.instance.protocolLib == true) {
+                    Bukkit.getConsoleSender().sendMessage(Main.instance.internalPrefix + "§aProtocolLib§e§l Mode! §c§lCommands are not supported in this mode!");
+                    return true;
+                }
+                if (args.length == 2) {
+                    if (args[0].equalsIgnoreCase("create")) {
+                        Main.instance.playerListHandler.addCustomOnlinePlayer(args[1]);
+                        Bukkit.getConsoleSender().sendMessage(Main.instance.internalPrefix + "§aPlayer successful created!");
+                        return true;
+                    }
+                    if (args[0].equalsIgnoreCase("remove")) {
+                        Main.instance.playerListHandler.removeCustomOnlinePlayer(args[1]);
+                        Bukkit.getConsoleSender().sendMessage(Main.instance.internalPrefix + "§aPlayer successful removed!");
+                        return true;
+                    }
+                }
+            }
+
+            if (cmd.getName().equalsIgnoreCase("help")) {
+                Bukkit.getConsoleSender().sendMessage("§aAvailable commands:");
+                Bukkit.getConsoleSender().sendMessage("§a/fakePlayers create <name> - Creates a fake player with the specified name.");
+                Bukkit.getConsoleSender().sendMessage("§a/fakePlayers remove <name> - Removes the fake player with the specified name.");
+                Bukkit.getConsoleSender().sendMessage("§a/help - Displays help information for FakePlayers plugin.");
+                return true;
+            }
+        }
+        return false;
+    }
 }
